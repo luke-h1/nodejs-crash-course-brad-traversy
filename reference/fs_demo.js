4a035551fd6f16179809867d5c5ab2137adce520
luke-h1 <luke.howsam@yahoo.com>
2020-08-12 20:23:37 +0100

    update

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,16 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 
-
-// create a folder 
+// create a folder
 // fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
-//   if (err) throw err; 
-//   console.log('Folder created'); 
+//   if (err) throw err;
+//   console.log('Folder created');
+// })
+
+// create and write to a file + append the the created file 
+// overwrites whatever is in the file 
+// fs.writeFile(
+//     path.join(__dirname, '/test', 'hello.txt'),
+//     'this is where content in the file goes',
+//     (err) => {
+//         if (err) throw err;
+//         console.log('file created');
+// // callback function below: 
+//         fs.appendFile(
+//             path.join(__dirname, '/test', 'hello.txt'),
+//             ' I love NodeJS',
+//             (err) => {
+//                 if (err) throw err;
+//                 console.log('file created');
+//             }
+//         );
+//     }
+// );
+
+
+// Read file 
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
 // })
 
 
-// create and write to a file 
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),'this is where content in the file goes' , (err) => {
-  if (err) throw err; 
-  console.log('file created'); 
+// rename a file 
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), (err) => {
+  if (err) throw err;
+  console.log('file renamed....');
 })
